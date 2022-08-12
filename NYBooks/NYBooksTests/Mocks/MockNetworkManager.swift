effaa0a16c656189b8a30b29d7a92cc1ff4b7448
PadraicBurns1 <110997699+PadraicBurns1@users.noreply.github.com>
2022-08-12 10:07:11 -0500

    Update MockNetworkManager.swift

--- NYBooks/NYBooksTests/Mocks/MockNetworkManager.swift
+++ NYBooks/NYBooksTests/Mocks/MockNetworkManager.swift
@@ -11,16 +11,6 @@ import Combine
 
 public class MockNetworkManager: NetworkFetcher {
     
-    let session: URLSession
-    
-    public init(session: URLSession = URLSession.shared) {
-        self.session = session
-    }
-    
-}
-
-extension MockNetworkManager {
-    
     public func getModel<T: Decodable>(url: URL?) -> AnyPublisher<T, Error> {
         return Just(NetworkData.getMock() as! T).setFailureType(to: Error.self).eraseToAnyPublisher()
     }
